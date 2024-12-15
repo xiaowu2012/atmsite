34d5fedccb35b99d6a852ce0a2f75c3adb4933e6
Linghua(Hut) <mmchut.wang@gmail.com>
2024-12-15 19:16:07 +0800

    adjustment

--- src/main/java/com/xiaowudr/atmsite/controller/UserController.java
+++ src/main/java/com/xiaowudr/atmsite/controller/UserController.java
@@ -63,38 +63,17 @@ public class UserController {
         Account targetAccount = userService.getUserByAccount(account.getAccountID());
         // it is not used, and target user exists
         if (targetAccount == null) {
-            RegistrationCodes registrationCodes = registrationCodesService.getRegistrationCodes(account.getRegisterCode());
-            if (registrationCodes != null) {
-                boolean isGoodToUse = registrationCodes.getIsUsed() == 0;
-                if (isGoodToUse) {
-                    userService.registerUser(account);
-                    if (registrationCodes.getRegisterType() == 1) {
-                        AccountTime accountTime = new AccountTime();
-                        accountTime.setAccount(account.getAccountID());
-                        accountTime.setDays(30);
-                        accountTime.setAccountType(1);
-                        accountTime.setTimeRemain(120);
-                        accountTimeService.insertAccountTime(accountTime);
-                    } else {
-                        AccountTime accountTime = new AccountTime();
-                        accountTime.setAccount(account.getAccountID());
-                        accountTime.setDays(1);
-                        accountTime.setAccountType(0);
-                        accountTime.setTimeRemain(21600);
-                        accountTimeService.insertAccountTime(accountTime);
-                    }
-                    registrationCodes.setIsUsed(1);
-                    registrationCodesService.updateIsUsed(registrationCodes);
-                    model.addAttribute("account", account);
-                    return "success";
-                } else {
-                    model.addAttribute("message", "错误：注册码已经使用。");
-                    return "register";
-                }
-            } else {
-                model.addAttribute("message", "错误：无效的注册码。");
-                return "register";
-            }
+            userService.registerUser(account);
+            AccountTime accountTime = new AccountTime();
+            accountTime.setAccount(account.getAccountID());
+            accountTime.setDays(1);
+            accountTime.setAccountType(0);
+            accountTime.setTimeRemain(180);
+            accountTimeService.insertAccountTime(accountTime);
+            //registrationCodes.setIsUsed(1);
+            //registrationCodesService.updateIsUsed(registrationCodes);
+            model.addAttribute("account", account);
+            return "success";
         } else {
             model.addAttribute("message", "错误：账号已经注册。");
             return "register";
