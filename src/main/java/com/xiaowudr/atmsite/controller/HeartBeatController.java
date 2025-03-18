ba7a9f16b23a7635ea48739b23851d3f5afa333c
Linghua(Hut) <mmchut.wang@gmail.com>
2025-03-18 21:45:21 +0800

    Fix some issues

--- src/main/java/com/xiaowudr/atmsite/controller/HeartBeatController.java
+++ src/main/java/com/xiaowudr/atmsite/controller/HeartBeatController.java
@@ -2,6 +2,9 @@ package com.xiaowudr.atmsite.controller;
 
 import com.xiaowudr.atmsite.mapper.AccountLockMapper;
 import com.xiaowudr.atmsite.pojo.AccountLock;
+import com.xiaowudr.atmsite.service.UserAccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 public class HeartBeatController {
     @Autowired
     private AccountLockMapper accountLockMapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(HeartBeatController.class);
+
 
     @PostMapping("/heartbeat")
     public void handleFileUpload(
@@ -22,6 +27,8 @@ public class HeartBeatController {
         //newUserAccountService.updateantitimestamp(playerID); //7b9e5f745e130536
         // fos.close();
         // Optionally, you can return a success message in the response
+
+        LOGGER.info("Heartbeat received for account: " + accountId);
 
         AccountLock accountLock = accountLockMapper.getAccountLock(accountId);
         if(accountLock == null) {
@@ -34,6 +41,9 @@ public class HeartBeatController {
             accountLockMapper.insertAccountLock(newAccountLock);
         } else {
             // update the value
+           /* if("".equals(accountLock.getExecPath()) || accountLock.getExecPath() == null) {
+                accountLock.setExecPath("initialized");
+            }*/
             accountLockMapper.updateAccountLock(accountLock);
         }
 
